b273b1d7e02d18bb26ee4de630213ed44164a1cf
mladenilic23 <67113935+mladenilic23@users.noreply.github.com>
2023-10-29 02:36:14 +0200

    Update zadatak3.s

--- vezbe1/zadatak3.s
+++ vezbe1/zadatak3.s
@@ -1,14 +1,18 @@
-addi x10, x0, 7       # x10 = 7
-addi x11, x0, 3       # x11 = 3
+li a0, 2
+li a1, 10  
+li a2, 4   
 
-#  SLL
-sll x12, x10, x11  # x12 = 7 << 3, x12 = 56
+mv a3, a0
 
-# SRL
-srl x13, x10, x11  # x13 = 7 >> 3, x13 = 0
+jal minimum
 
-# XOR
-xor x14, x10, x11  # x14 = 7 ^ 3, x14 = 4
+minimum:
+    blt a3, a1, check_a1_a2
+    mv a3, a1  # Ako je a1 manji, postavi a1 kao minimum
 
-# SLT
-slt x15, x11, x10  # x15 = (3 < 7) ? 1 : 0, x15 = 1
+check_a1_a2:
+    blt a3, a2, minimum_done
+    mv a3, a2  # Ako je a2 manji, postavi a2 kao minimum
+
+minimum_done:
+    jalr x0, x1, 0
